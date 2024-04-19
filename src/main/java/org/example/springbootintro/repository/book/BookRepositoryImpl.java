2a8ade6530e56b9b353b0fc39cc37763db01bdfe
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-04-19 21:53:40 +0300

    fix: 1 add books classes

--- src/main/java/org/example/springbootintro/repository/book/BookRepositoryImpl.java
+++ src/main/java/org/example/springbootintro/repository/book/BookRepositoryImpl.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class BookRepositoryImpl implements BookRepository{
+public class BookRepositoryImpl implements BookRepository {
     private final SessionFactory sessionFactory;
 
     @Autowired
